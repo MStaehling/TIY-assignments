913e2e81277be102836b4d9a75964424030249a6
Michael Staehling <Michael.staehling@gmail.com>
2015-05-07 16:07:43 -0400

    initial code for SC

---
+++ string-calculator.js
@@ -0,0 +1,38 @@
+var expect = require('chai').expect;
+
+function toEnglish(word){
+  if ( word === "zero" ){
+    return 0;
+  }
+  if ( word === "one" ){
+    return 1;
+  }
+  if ( word === "two" ){
+    return 2;
+  }
+  if ( word === "three" ){
+    return 3;
+  }
+  if ( word === "four" ){
+    return 4;
+  }
+  if ( word === "five" ){
+    return 5;
+  }
+  if ( word === "six" ){
+    return 6;
+  }
+  if ( word === "seven" ){
+    return 7;
+  }
+  if ( word === "eight" ){
+    return 8;
+  }
+  if ( word === "nine" ){
+    return 9;
+  }
+
+}
+
+expect(toEnglish("zero")).to.equal(0);
+expect(toEnglish("one")).to.equal(1);
