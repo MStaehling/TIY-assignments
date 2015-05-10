ebbcd44272bac15dbdbbac227b14ca9526ec7ffd
Michael Staehling <Michael.staehling@gmail.com>
2015-05-10 13:59:03 -0400

    multiply done

--- string-calculator.js
+++ string-calculator.js
@@ -1088,7 +1088,407 @@ expect(sub("nine", "eight")).to.equal(1);
 expect(sub("nine", "nine")).to.equal(0);
 
 function multiply(a, b){
-
+  if ( a === "zero" && b === "zero"){
+    var a = 0;
+    var b = 0;
+  }
+  if ( a ==== "zero" && b === "one"){
+    var a = 0;
+    var b = 1;
+  }
+  if ( a === "zero" && b === "two"){
+    var a = 0;
+    var b = 2;
+  }
+  if ( a === "zero" && b === "three"){
+    var a = 0;
+    var b = 3;
+  }
+  if ( a === "zero" && b === "four"){
+    var a = 0;
+    var b = 4;
+  }
+  if ( a === "zero" && b === "five"){
+    var a = 0;
+    var b = 5;
+  }
+  if ( a === "zero" && b === "six"){
+    var a = 0;
+    var b = 6;
+  }
+  if ( a === "zero" && b === "seven"){
+    var a = 0;
+    var b = 7;
+  }
+  if ( a === "zero" && b === "eight"){
+    var a = 0;
+    var b = 8;
+  }
+  if ( a === "zero" && b === "nine"){
+    var a = 0;
+    var b = 9;
+  }
+  if ( a === "one" && b === "zero"){
+    var a = 1;
+    var b = 0;
+  }
+  if ( a === "one" && b === "one"){
+    var a = 1;
+    var b = 1;
+  }
+  if ( a === "one" && b === "two"){
+    var a = 1;
+    var b = 2;
+  }
+  if ( a === "one" && b === "three"){
+    var a = 1;
+    var b = 3;
+  }
+  if ( a === "one" && b === "four"){
+    var a = 1;
+    var b = 4;
+  }
+  if ( a === "one" && b === "five"){
+    var a = 1;
+    var b = 5;
+  }
+  if ( a === "one" && b === "six"){
+    var a = 1;
+    var b = 6;
+  }
+  if ( a === "one" & b === "seven"){
+    var a = 1;
+    var b = 7;
+  }
+  if ( a === "one" && b === "eight"){
+    var a = 1;
+    var b = 8;
+  }
+  if ( a === "one" && b === "nine"){
+    var a = 1;
+    var b = 9;
+  }
+  if ( a === "two" && b === "zero"){
+    var = 2;
+    var = 0;
+  }
+  if ( a === "two" && b === "one"){
+    var a = 2;
+    var b = 1;
+  }
+  if ( a === "two" && b === "two"){
+    var a = 2;
+    var b = 2;
+  }
+  if (a === "two" && b === "three"){
+    var a = 2;
+    var b = 3;
+  }
+  if ( a === "two" && b === "four"){
+    var a = 2;
+    var b = 4;
+  }
+  if ( a === "two" && b === "five"){
+    var a = 2;
+    var b = 5;
+  }
+  if ( a === "two" && b === "six"){
+    var a = 2;
+    var b = 6;
+  }
+  if ( a === "two" && b === "seven"){
+    var a = 2;
+    var b = 7;
+  }
+  if ( a === "two" && b === "eight"){
+    var a = 2;
+    var b = 8;
+  }
+  if ( a === "two" && b === "nine"){
+    var a = 2;
+    var b = 9;
+  }
+  if ( a === "three" && b === "zero"){
+    var a = 3;
+    var b = 0;
+  }
+  if ( a === "three" && b === "one"){
+    var a = 3;
+    var b = 1;
+  }
+  if ( a === "three" && b === "two"){
+    var a = 3;
+    var b = 2;
+  }
+  if ( a === "three" && b === "three"){
+    var a = 3;
+    var b = 3;
+  }
+  if ( a === "three" && b === "four"){
+    var a = 3;
+    var b = 4;
+  }
+  if ( a === "three" && b === "five"){
+    var a = 3;
+    var b = 5;
+  }
+  if ( a === "three" && b === "six"){
+    var a = 3;
+    var b = 6;
+  }
+  if ( a === "three" && b === "seven"){
+    var a = 3;
+    var b = 7;
+  }
+  if ( a === "three" && b === "eight"){
+    var a = 3;
+    var b = 8;
+  }
+  if ( a === "three" && b === "nine"){
+    var a = 3;
+    var b = 9;
+  }
+  if ( a === "four" && b === "zero"){
+    var a = 4;
+    var b = 0;
+  }
+  if ( a === "four" && b === "one"){
+    var a = 4;
+    var b = 1;
+  }
+  if ( a === "four" && b === "two"){
+    var a = 4;
+    var b = 2;
+  }
+  if ( a === "four" && b === "three"){
+    var a = 4;
+    var b = 3;
+  }
+  if ( a === "four" && b === "four"){
+    var a = 4;
+    var b = 4;
+  }
+  if ( a === "four" && b === "five"){
+    var a = 4;
+    var b = 5;
+  }
+  if ( a === "four" && b === "six"){
+    var a = 4;
+    var b = 6;
+  }
+  if ( a === "four" && b === "seven"){
+    var a = 4;
+    var b = 7;
+  }
+  if ( a === "four" && b === "eight"){
+    var a = 4;
+    var b = 8;
+  }
+  if ( a === "four" && b === "nine"){
+    var a = 4;
+    var b = 9;
+  }
+  if ( a === "five" && b === "zero"){
+    var a = 5;
+    var b = 0;
+  }
+  if ( a === "five" && b === "one"){
+    var a = 5;
+    var b = 1;
+  }
+  if ( a === "five" && b === "two"){
+    var a = 5;
+    var b = 2;
+  }
+  if ( a === "five" && b === "three"){
+    var a = 5;
+    var b = 3;
+  }
+  if ( a === "five" && b === "four"){
+    var a = 5;
+    var b = 4;
+  }
+  if ( a === "five" && b === "five"){
+    var a = 5;
+    var b = 5;
+  }
+  if ( a === "five" && b === "six"){
+    var a = 5;
+    var b = 6;
+  }
+  if ( a === "five" && b === "seven"){
+    var a = 5;
+    var b = 7;
+  }
+  if ( a === "five" && b === "eight"){
+    var a = 5;
+    var b = 8;
+  }
+  if ( a === "five" && b === "nine"){
+    var a = 5;
+    var b = 9;
+  }
+  if ( a === "six" && b === "zero"){
+    var a = 6;
+    var b = 0;
+  }
+  if ( a === "six" && b === "one"){
+    var a = 6;
+    var b = 1;
+  }
+  if ( a === "six" && b === "two"){
+    var a = 6;
+    var b = 2;
+  }
+  if ( a === "six" && b === "three"){
+    var a = 6;
+    var b = 3;
+  }
+  if ( a === "six" && b === "four"){
+    var a = 6;
+    var b = 4;
+  }
+  if ( a === "six" && b === "five"){
+    var a = 6;
+    var b = 5;
+  }
+  if ( a === "six" && b === "six"){
+    var a = 6;
+    var b = 6;
+  }
+  if ( a === "six" && b === "seven"){
+    var a = 6;
+    var b = 7;
+  }
+  if ( a === "six" && b === "eight"){
+    var a = 6;
+    var b = 8;
+  }
+  if ( a === "six" && b === "nine"){
+    var a = 6;
+    var b = 9;
+  }
+  if ( a === "seven" && b === "zero"){
+    var a = 7;
+    var b = 0;
+  }
+  if ( a === "seven" && b === "one"){
+    var a = 7;
+    var b = 1;
+  }
+  if ( a === "seven" && b === "two"){
+    var a = 7;
+    var b = 2;
+  }
+  if ( a === "seven" && b === "three"){
+    var a = 7;
+    var b = 3;
+  }
+  if ( a === "seven" && b === "four"){
+    var a = 7;
+    var b = 4;
+  }
+  if ( a === "seven" && b === "five"){
+    var a = 7;
+    var b = 5;
+  }
+  if ( a === "seven" && b === "six"){
+    var a = 7;
+    var b = 6;
+  }
+  if ( a === "seven" && b === "seven"){
+    var a = 7;
+    var b = 7;
+  }
+  if ( a === "seven" && b === "eight"){
+    var a = 7;
+    var b = 8;
+  }
+  if ( a === "seven" && b === "nine"){
+    var a = 7;
+    var b = 9;
+  }
+  if ( a === "eight" && b === "zero"){
+    var a = 8;
+    var b = 0;
+  }
+  if ( a === "eight" && b === "one"){
+    var a = 8;
+    var b = 1;
+  }
+  if ( a === "eight" && b === "two"){
+    var a = 8;
+    var b = 2;
+  }
+  if ( a === "eight" && b === "three"){
+    var a = 8;
+    var b = 3;
+  }
+  if ( a === "eight" && b === "four"){
+    var a = 8;
+    var b = 4;
+  }
+  if ( a === "eight" && b === "five"){
+    var a = 8;
+    var b = 5;
+  }
+  if ( a === "eight" && b === "six"){
+    var a = 8;
+    var b = 6;
+  }
+  if ( a === "eight" && b === "seven"){
+    var a = 8;
+    var b = 7;
+  }
+  if ( a === "eight" && b === "eight"){
+    var a = 8;
+    var b = 8;
+  }
+  if ( a === "eight" && b === "nine"){
+    var a = 8;
+    var b = 9;
+  }
+  if ( a === "nine" && b === "zero"){
+    var a = 9;
+    var b = 0;
+  }
+  if ( a === "nine" && b === "one"){
+    var a = 9;
+    var b = 1;
+  }
+  if ( a === "nine" && b === "two"){
+    var a = 9;
+    var b = 2;
+  }
+  if ( a === "nine" && b === "three"){
+    var a = 9;
+    var b = 3;
+  }
+  if ( a === "nine" && b === "four"){
+    var a = 9;
+    var b = 4;
+  }
+  if ( a === "nine" && b === "five"){
+    var a = 9;
+    var b = 5;
+  }
+  if ( a === "nine" && b === "six"){
+    var a = 9;
+    var b = 6;
+  }
+  if ( a === "nine" && b === "seven"){
+    var a = 9;
+    var b = 7;
+  }
+  if ( a === "nine" && b === "eight"){
+    var a = 9;
+    var b = 8;
+  }
+  if ( a === "nine" && b === "nine"){
+    var a = 9;
+    var b = 9;
+  }
+  return a * b
 }
 
 expect(multiply).to.exist;
