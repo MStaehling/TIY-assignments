b94e12750ddd20b7f3b7574ea33986e7ef35a21a
Michael Staehling <Michael.staehling@gmail.com>
2015-05-12 19:41:47 -0400

    class work

---
+++ NestedAccordian/scripts.js
@@ -0,0 +1,25 @@
+/*alert("It's Alive!");*/
+//debugger;
+
+
+document.head.parentElement.className = "js"; // It's Alive!
+
+
+var elements = document.querySelectorAll("h3");
+
+
+//WTF JS>>>>WTF!
+//for ( var index = 0; index < elements.length; index++){
+  //element  == elements[index]
+//  elements[index].addEventListener('click', function(){
+  //  console.log(elements, index, elements)
+  //   elements[index].parentElement.classList.toggle('cbp-ntopen');
+    // })
+//
+
+_.forEach(elements, function(element, index, elements){
+  element.addEventListener('click', function(){
+    console.log(elements, index, elements);
+     element.parentElement.classList.toggle('cbp-ntopen');
+   });
+});
