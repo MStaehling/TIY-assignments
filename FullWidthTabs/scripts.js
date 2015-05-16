18acb4c82fd6234bd6bf87002c15abdce6291c9e
Michael Staehling <Michael.staehling@gmail.com>
2015-05-16 16:31:17 -0400

    promising leads to show tab info

--- FullWidthTabs/scripts.js
+++ FullWidthTabs/scripts.js
@@ -1,9 +1,17 @@
 document.head.parentElement.className = "js";
 
-var elements = document.querySelectorAll('.tabs');
+var elements = document.querySelectorAll('a');
+
+var panelElements = document.querySelectorAll('h3');
 
 _.forEach(elements, function(element, index, elements){
   element.addEventListener('click', function(){
-    element.parentElement.classList.toggle('.tab-current');
-  })
-})
+    element.parentElement.classList.toggle('tab-current');
+  });
+});
+
+_.forEach(panelElements, function(element, index, panelElements){
+  element.addEventListener('visibilitychange', function(){
+    element.parentElement.classList.toggle('.content-current');
+  });
+});
