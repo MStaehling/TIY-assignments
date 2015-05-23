58136ea6a19f2656716e47097d44de6392f17f1f
Michael Staehling <Michael.staehling@gmail.com>
2015-05-23 17:08:05 -0400

    got tabs to work with @sjoyal

--- FullWidthTabs/scripts.js
+++ FullWidthTabs/scripts.js
@@ -1,5 +1,6 @@
 (function(window) {
   var BASE_URL = 'http://lorempixel.com/350/200/abstract';
+
   function rando(limit) {
     return Math.floor(Math.random() * limit);
   }
@@ -11,3 +12,153 @@
 
   $('img').attr('src', randoImage);
 })(window);
+
+var tab1 = document.querySelector('.tab1');
+var tab2 = document.querySelector('.tab2');
+var tab3 = document.querySelector('.tab3');
+var tab4 = document.querySelector('.tab4');
+var tab5 = document.querySelector('.tab5');
+var Section1 = document.querySelector('#section-1');
+var Section2 = document.querySelector('#section-2');
+var Section3 = document.querySelector('#section-3');
+var Section4 = document.querySelector('#section-4');
+var Section5 = document.querySelector('#section-5');
+
+
+var listItems = document.querySelectorAll('li');
+var contentBlock = document.querySelectorAll('section');
+
+
+
+tab1.addEventListener('click', function() {
+  _.forEach(listItems, function(element) {
+    if (element.className === 'tab-current') {
+      element.classList.remove('tab-current');
+    };
+  });
+  _.forEach(contentBlock, function(element) {
+    if (element.className === 'content-current') {
+      element.classList.remove('content-current');
+    };
+  });
+  this.parentNode.classList.add('tab-current');
+  Section1.classList.add('content-current');
+});
+
+tab2.addEventListener('click', function() {
+  _.forEach(listItems, function(element) {
+    if (element.className === 'tab-current') {
+      element.classList.remove('tab-current');
+    };
+  });
+  _.forEach(contentBlock, function(element) {
+    if (element.className === 'content-current') {
+      element.classList.remove('content-current');
+    };
+  });
+  this.parentNode.classList.add('tab-current');
+  Section2.classList.add('content-current');
+});
+
+tab3.addEventListener('click', function() {
+  _.forEach(listItems, function(element) {
+    if (element.className === 'tab-current') {
+      element.classList.remove('tab-current');
+    };
+  });
+  _.forEach(contentBlock, function(element) {
+    if (element.className === 'content-current') {
+      element.classList.remove('content-current');
+    };
+  });
+  this.parentNode.classList.add('tab-current');
+  Section3.classList.add('content-current');
+});
+
+tab4.addEventListener('click', function() {
+  _.forEach(listItems, function(element) {
+    if (element.className === 'tab-current') {
+      element.classList.remove('tab-current');
+    };
+  });
+  _.forEach(contentBlock, function(element) {
+    if (element.className === 'content-current') {
+      element.classList.remove('content-current');
+    };
+  });
+  this.parentNode.classList.add('tab-current');
+  Section4.classList.add('content-current');
+});
+
+tab5.addEventListener('click', function() {
+  _.forEach(listItems, function(element) {
+    if (element.className === 'tab-current') {
+      element.classList.remove('tab-current');
+    };
+  });
+  _.forEach(contentBlock, function(element) {
+    if (element.className === 'content-current') {
+      element.classList.remove('content-current');
+    };
+  });
+  this.parentNode.classList.add('tab-current');
+  Section5.classList.add('content-current');
+});
+
+//removeClass(element, "foo");element.className = "foo";
+
+// tab2.$('click', function(){
+//   $('li').removeClass('tab-current');
+//   tab2.addClass('tab-current');
+// });
+
+// $('li').each(function(){
+//   $('li').removeClass('tab-current');
+//   $('li').addClass('tab-current');
+// })
+
+//THIS WORKS BUT DON'T COMPLETELY UNDERSTAND Y
+// $('ul').each(function(){
+//     // For each set of tabs, we want to keep track of
+//     // which tab is active and it's associated content
+//     var $active, $content, $links = $(this).find('a');
+//
+//     // If the location.hash matches one of the links, use that as the active tab.
+//     // If no match is found, use the first link as the initial active tab.
+//     $active = $($links.filter('[href="'+location.hash+'"]')[0] || $links[0]);
+//     $active.addClass('active');
+//
+//     $content = $($active[0].hash);
+//
+//     // Hide the remaining content
+//     $links.not($active).each(function () {
+//       $(this.hash).hide();
+//     });
+//
+//     // Bind the click event handler
+//     $(this).on('click', 'a', function(e){
+//       // Make the old tab inactive.
+//       $active.removeClass('active');
+//       $content.hide();
+//
+//       // Update the variables with the new link and content
+//       $active = $(this);
+//       $content = $(this.hash);
+//
+//       // Make the tab active.
+//       $active.addClass('active');
+//       $content.show();
+//
+//       // Prevent the anchor's default click action
+//       e.preventDefault();
+//     });
+//   });
+
+
+
+//$('#MyElement').toggleClass('MyClass');
+// $('#MyElement').addClass('MyClass');
+//
+// $('#MyElement').removeClass('MyClass');
+//
+// if ( $('#MyElement').hasClass('MyClass') )
